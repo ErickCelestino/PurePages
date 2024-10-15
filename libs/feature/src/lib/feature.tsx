325c56579afc288dab548f6e8c59e6e212a25173
ErickEmpresa <erickcelestimo@gmail.com>
2024-10-15 10:46:06 -0300

    chore: configure feat react library initial setup

---
+++ libs/feature/src/lib/feature.tsx
@@ -0,0 +1,11 @@
+import styles from './feature.module.scss';
+
+export function Feature() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Feature!</h1>
+    </div>
+  );
+}
+
+export default Feature;
